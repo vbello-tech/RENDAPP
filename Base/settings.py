419238ae378500d3ee1c699ea743e8abfc5f5129
vbello-tech <odunayobello1@gmail.com>
2022-07-04 14:38:47 +0100

    models

--- Base/settings.py
+++ Base/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2^@9tklt7k+6(d^gnllx)n13i_l6kvy!kll6=rn%ovi!4)vv^0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Base/settings/development.py
+++ Base/settings/development.py
@@ -5,10 +5,10 @@ from .base import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
