c77f80473ceffaaa2d20234d1436324be4862bbc
vbello-tech <odunayobello1@gmail.com>
2022-07-05 14:11:09 +0100

    admin

--- Base/urls.py
+++ Base/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('services.urls', namespace="service")),
     path('user/', include('users.urls', namespace="user")),
-    #path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
