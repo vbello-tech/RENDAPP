a0950e092bc43231cc2f933ddda0186d6d93caf3
vbello-tech <odunayobello1@gmail.com>
2022-08-20 12:20:19 +0100

    railway

--- Rendapp/settings/production.py
+++ Rendapp/settings/production.py
@@ -2,12 +2,24 @@ from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+"""
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD': 'V0IBvvzHtbxwylN9xL75',
+       'HOST': 'containers-us-west-42.railway.app',
+       'PORT': '6652',
+   }
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
